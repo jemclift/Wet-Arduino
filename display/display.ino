39d2f40f3787f111cdf1fcaafc8a4e15decc7dbe
jemclift <->
2021-12-12 13:11:57 +0000

    Commented display.ino

--- display/display.ino
+++ display/display.ino
@@ -8,12 +8,14 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 // other pins
 const int btn_pin = 2, led_pin = 7, bzr_pin = 13;
 
+// warning variables
 bool dispayWarning = false;
 unsigned long nextWarningFlash = 0;
 int warningFlashDelay = 500; // milliseconds
 bool warningFlashState = true;
-int warningCode = 0;
 bool canBeWarning[3] = {true, true, true};
+
+int warningCode = 0;
 
 // 0 - No Warning
 // 1 - Too Dry
@@ -24,6 +26,7 @@ int temperature = 20;
 int lightLevel = 0;
 int moisture = 50;
 
+// custom degree character
 byte degree[8] = {
   0b11100,
   0b10100,
@@ -36,33 +39,42 @@ byte degree[8] = {
 };
 
 void setup() {
+
+  // initialise LCD
   lcd.begin(16, 2);
+
+  // load custom character
   lcd.createChar(0, degree);
+
+  // assign pins and button interupt
   pinMode(led_pin, OUTPUT);
   pinMode(btn_pin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(btn_pin), buttonPress, FALLING);
 
+  // connect to master arduino
   Wire.begin(A5);
   Wire.onReceive(receiveEvent);
 }
 
+// used to turn off warning when values return to normal
 void turnOffWarning() {
   dispayWarning = false;
   warningCode = 0;
   digitalWrite(led_pin, LOW);
 }
 
+// recieve data from master arduino
 void receiveEvent(int howMany) {
+
+  // read transmitted sensor data
   temperature = Wire.read();
   lightLevel = Wire.read();
   moisture = Wire.read();
-  while(Wire.available() > 0){
+
+  // clear extra bytes
+  while (Wire.available() > 0) {
     Wire.read();
   }
-
-  // 1 - Too Dry
-  // 2 - Too Hot
-  // 3 - Too Cold
 
   // warning checks
   if (moisture < 20) {
@@ -101,6 +113,7 @@ void receiveEvent(int howMany) {
   }
 }
 
+// display given sensor data on LCD
 void updateLCD(int temp, int soil_hum, int light) {
   lcd.setCursor(0, 0);
   lcd.print("TEMP:"); lcd.print(temp); lcd.write(byte(0));
@@ -110,13 +123,19 @@ void updateLCD(int temp, int soil_hum, int light) {
   lcd.print("       ");
 }
 
+// displays a flashing warning
 void warningMsg() {
   unsigned long currentTime = millis();
 
+  // if it's time to flip warning flash state
   if (currentTime > nextWarningFlash) {
+
+    // flip warning flash state
     warningFlashState = !warningFlashState;
 
     digitalWrite(led_pin, warningFlashState);
+
+    // write common top line of display
     lcd.setCursor(0, 0);
     if (warningFlashState) {
       tone(bzr_pin, 440, 100);
@@ -125,6 +144,7 @@ void warningMsg() {
       lcd.print("    WARNING!    ");
     }
 
+    // write specific bottom line of display
     lcd.setCursor(0, 1);
     switch(warningCode) {
       case 1:
@@ -134,10 +154,13 @@ void warningMsg() {
       case 3:
         lcd.print("Too cold"); break;
     }
+
+    // set next flash time
     nextWarningFlash = currentTime + warningFlashDelay;
   }
 }
 
+// turn off warning if button is pressed
 void buttonPress() {
   dispayWarning = false;
   canBeWarning[warningCode-1] = false;
