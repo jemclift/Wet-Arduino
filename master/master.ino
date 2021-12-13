258d0c883c090cf90fa3fd83367420df8435e087
JoshuaPeachey <joshuajpeachey@gmail.com>
2021-12-13 18:39:44 +0000

    Commented and finished off the code.

--- master/master.ino
+++ master/master.ino
@@ -6,13 +6,10 @@
 #define tempPin A1
 #define lightSensorPin A2
 
-float soilCheckDelay = 0.01; // minutes
+float soilCheckDelay = 30; // This specifies how often the soil should be checked
 unsigned long nextSoilCheck = 0;
-float sensorCheckDelay = 1; // minutes
+float sensorCheckDelay = 10; // This specifies how often the temperature, light and soil moisture Percentage should be updated and sent to the LCD on the slave Ardunio.
 unsigned long nextSensorCheck = 0;
-int numOfSoilReadings = 0; // This keeps track of the number of times the soil moisture has been checked.
-int totalSoilMoisturePercentage = 0; //The soil percentage is added to this and then divided by the amount of checks.
-int numOfSoilReadingsBeforeCheck = 10;
 
 // readings
 double temperature = 0;
@@ -36,6 +33,7 @@ void setup() {
 
 
 void manualButtonPump(){
+  // This function pumps water as long as the button is held down.
   int buttonState = HIGH;
   digitalWrite(waterPumpPin, HIGH);
 
@@ -49,6 +47,7 @@ void manualButtonPump(){
 
 
 void sendToSlave() {
+  // This function sends the data recorded to the slave arudino so it can be shown on the screen.
   Wire.beginTransmission(A5);
   byte message[] = {(byte)temperature, lightLevel, soilMoisturePercentage};
   int message_length = 10;
@@ -58,6 +57,7 @@ void sendToSlave() {
 
 
 void activatePump(){
+  // This function activates the pump for 2 secs to pump a small amount of water into the plant.
   Serial.println("Water Pump Activated");
   digitalWrite(waterPumpPin, HIGH);
   unsigned long currentTime = millis();
@@ -71,15 +71,16 @@ void activatePump(){
 
 
 int readLightSensor() {
+  // This function returns a percentage value stating how much light the LDR is receiving.
   int lightReading = analogRead(lightSensorPin);
   return map(lightReading, 0, 1024, 0, 100);
 }
 
 
-// returns temp in degrees c
+
 double readTemperatureSensor() {
+  // This function returns the temperature in degrees celcius.
   int sensorReading = analogRead(tempPin);
-  // return ((double)sensorReading-102.4) * 125/256;
   double temp = (double)sensorReading / 1024;
   temp = temp * 5;
   temp = temp - 0.5;
@@ -89,6 +90,7 @@ double readTemperatureSensor() {
 
 
 int readSoilMoisture(){
+  // This function returns the percentage moisture of the soil.
   digitalWrite(sensorPinPower, HIGH);
   delay(10);
   int val = analogRead(sensorPinRead);
@@ -97,39 +99,57 @@ int readSoilMoisture(){
   return val;
 }
 
+void checkSoilMoisturePercentage(){
+  // This function records the percentage moisture of the soil 10 times and then waters the plant if the value is less than 50%
+  int numOfSoilReadings = 0;
+  int numOfSoilReadingsBeforeCheck = 10; //This specifies the number of readings taken before the average reading is worked out.
+  int totalSoilMoisturePercentage;
+  int averageSoilMoisturePercentage;
 
-void loop() {
-  unsigned long currentTime = millis();
-  bool sendData = false;
-  // water plant if it's too dry every soilCheckDelay mins
-  if (currentTime > nextSoilCheck) {
-    sendData = true;
+  while (numOfSoilReadings < numOfSoilReadingsBeforeCheck){
+    numOfSoilReadings +=1;
+    // This retrieves the soil Moisture Percentage and prints it out to the serial.
     soilMoisturePercentage = readSoilMoisture();
     Serial.print("Water Moisture level: ");
     Serial.print(soilMoisturePercentage);
     Serial.println("%");
+
     totalSoilMoisturePercentage = soilMoisturePercentage + totalSoilMoisturePercentage;
-    numOfSoilReadings +=1;
-    if ((numOfSoilReadings % numOfSoilReadingsBeforeCheck == 0) and (numOfSoilReadings != 0)){
-      int averageSoilMoisturePercentage = totalSoilMoisturePercentage / numOfSoilReadingsBeforeCheck;
+    if (numOfSoilReadings == numOfSoilReadingsBeforeCheck){
+      averageSoilMoisturePercentage = totalSoilMoisturePercentage / numOfSoilReadingsBeforeCheck;
       totalSoilMoisturePercentage = 0;
+
+      // This part checks if the soil moisture is over 50%. If it isn't the plant will be water
       Serial.println("Check Soil Moisture Percentage");
       if (averageSoilMoisturePercentage < 50) {
         activatePump();
-        // Serial.println("activated Pump");
       }
     }
-    nextSoilCheck = currentTime + (soilCheckDelay*60*1000);
   }
-  // send temperature and light readings to other arduino
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+  bool sendData = false;
+
+  // Checks to see if the plant should be watered and then works out at what time the plant should be checked again.
+  if (currentTime > nextSoilCheck) {
+    sendData = true;
+    checkSoilMoisturePercentage();
+    nextSoilCheck = currentTime + (soilCheckDelay*60*1000);
+      }
+
   if (currentTime > nextSensorCheck) {
+    // Gets new values from all of the sensors.
     sendData = true;
     lightLevel = readLightSensor();
     temperature = readTemperatureSensor();
+    soilMoisturePercentage = readSoilMoisture();
 
     nextSensorCheck = currentTime + (sensorCheckDelay*60*1000);
   }
 
+  // if the new values from the sensor have been calculated it will send these to the other arduino to be shown on the LCD.
   if (sendData){
     sendToSlave();
   }
